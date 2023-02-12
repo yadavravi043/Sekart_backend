6777203395ff2a5b1d586a7c362ec05040a4e975
yadavravi043 <ravi18016925@gmail.com>
2023-02-12 23:34:15 +0530

    update controller

--- controller/category.js
+++ controller/category.js
@@ -58,6 +58,56 @@ exports.getCategories = (req, res) => {
       }
     });
   };
-  exports.updateCategories=(req,res)=>{
-    res.status(200).json("updatedcategies")
-  }
+  exports.updateCategories = async (req, res) => {
+    const { _id, name, parentId, type } = req.body;
+    const updatedCategories = [];
+    if (name instanceof Array) {
+      for (let i = 0; i < name.length; i++) {
+        const category = {
+          name: name[i],
+          type: type[i],
+        };
+        if (parentId[i] !== "") {
+          category.parentId = parentId[i];
+        }
+  
+        const updatedCategory = await Category.findOneAndUpdate(
+          { _id: _id[i] },
+          category,
+          { new: true }
+        );
+        updatedCategories.push(updatedCategory);
+      }
+      return res.status(201).json({ updateCategories: updatedCategories });
+    } else {
+      const category = {
+        name,
+        type,
+      };
+      if (parentId !== "") {
+        category.parentId = parentId;
+      }
+      const updatedCategory = await Category.findOneAndUpdate({ _id }, category, {
+        new: true,
+      });
+      return res.status(201).json({ updatedCategory });
+    }
+  };
+  
+  exports.deleteCategories = async (req, res) => {
+    const { ids } = req.body.payload;
+    const deletedCategories = [];
+    for (let i = 0; i < ids.length; i++) {
+      const deleteCategory = await Category.findOneAndDelete({
+        _id: ids[i]._id,
+        createdBy: req.user._id,
+      });
+      deletedCategories.push(deleteCategory);
+    }
+  
+    if (deletedCategories.length == ids.length) {
+      res.status(201).json({ message: "Categories removed" });
+    } else {
+      res.status(400).json({ message: "Something went wrong" });
+    }
+  };
