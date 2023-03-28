b1e3660b5c6a8181b7946afd6e53a1b4a6998bf3
yadavravi043 <ravi18016925@gmail.com>
2023-03-28 23:21:20 +0530

    fix

--- models/page.js
+++ models/page.js
@@ -25,7 +25,7 @@ const pageSchema = new mongoose.Schema({
     category: { 
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'Category', 
-        required: true ,
+        // required: true ,
         unique: true
     },
     createdBy: {
